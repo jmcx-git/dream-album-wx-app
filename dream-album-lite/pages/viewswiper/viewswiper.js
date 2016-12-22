9830f4baf18361c205e8b38c07ffe1ec5a9fb6b8
liuxinglong <liuxinglong@liuxinglongdeMacBook-Pro.local>
2016-12-22 15:14:11 +0800

    预览页修改

--- dream-album-lite/pages/viewswiper/viewswiper.js
+++ dream-album-lite/pages/viewswiper/viewswiper.js
@@ -1,96 +1,124 @@
-var app=getApp();
+var app = getApp();
 Page({
-  data:{
+  data: {
     indicatorDots: true,
     autoplay: true,
     interval: 3000,
     duration: 500,
-    winWidth:0,
-    winHeight:0,
-    currentTab:0,
-    pptHidden:false,
-    portHidden:true,
-    bigPreImg:'',
-    loopPreImgs:[],
-    bottomDisplay:'block',
-    intervalOver:true,
-    bottomHidden:false
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+    pptHidden: false,
+    portHidden: true,
+    bigPreImg: '',
+    loopPreImgs: [],
+    bottomDisplay: 'block',
+    intervalOver: true,
+    bottomHidden: false,
+    refresh: true,
+    refreshtip: ''
   },
-  onLoad:function(options){
-    let that=this;
+  onLoad: function (options) {
+    let that = this;
     that.setData({
-        winWidth:app.globalData.windowWidth,
-        winHeight:app.globalData.windowHeight
+      winWidth: app.globalData.windowWidth,
+      winHeight: app.globalData.windowHeight
     })
-    var albumId=options.albumId;
-    var userAlbumId=options.userAlbumId;
+    that.albumId = options.albumId;
+    that.userAlbumId = options.userAlbumId;
+    that.init()
+  },
+  init: function (e) {
+    let that = this;
+    that.setData({
+      refreshtip: ''
+    })
+    let albumId = that.albumId;
+    let userAlbumId = that.userAlbumId;
+    if (userAlbumId != undefined) {
+      wx.showToast({
+        title: '加载中',
+        icon: 'loading',
+        duration: 5000
+      })
+    }
     wx.request({
-      url: app.globalData.serverHost+'dream/album/common/getpreview.json?',
+      url: app.globalData.serverHost + 'dream/album/common/getpreview.json?',
       data: {
-        albumId:albumId==undefined?'':albumId,
-        userAlbumId:userAlbumId==undefined?'':userAlbumId
+        albumId: albumId == undefined ? '' : albumId,
+        userAlbumId: userAlbumId == undefined ? '' : userAlbumId
       },
       method: 'GET',
-      success: function(res){
-        that.setData({
-          loopPreImgs:res.data.loopPreImgs,
-          bigPreImg:res.data.bigPreImg
-        })
-        setTimeout(function(){
+      success: function (res) {
+        if (res.data.makeComplete) {
+          wx.hideToast()
           that.setData({
-            bottomDisplay:'none',
-            winHeight:that.data.winHeight+50,
-            intervalOver:false,
-            bottomHidden:true
+            refresh: false,
+            loopPreImgs: res.data.loopPreImgs,
+            bigPreImg: res.data.bigPreImg
           })
-        },3000)
+          setTimeout(function () {
+            that.setData({
+              bottomDisplay: 'none',
+              winHeight: that.data.winHeight + 50,
+              intervalOver: false,
+              bottomHidden: true
+            })
+          }, 3000)
+        } else {
+          setTimeout(function () {
+            that.setData({
+              refreshtip: '点击页面刷新'
+            })
+          }, 5000)
+        }
       }
     })
   },
-  swichNav:function(e){
+  swichNav: function (e) {
     this.setData({
-      currentTab:e.currentTarget.dataset.id,
-      pptHidden:e.currentTarget.dataset.id==1?true:false,
-      portHidden:e.currentTarget.dataset.id==0?true:false
+      currentTab: e.currentTarget.dataset.id,
+      pptHidden: e.currentTarget.dataset.id == 1 ? true : false,
+      portHidden: e.currentTarget.dataset.id == 0 ? true : false
     })
     wx.setNavigationBarTitle({
       title: e.currentTarget.dataset.title
     })
   },
-  saveImg:function(e){
+  saveImg: function (e) {
     wx.showActionSheet({
-      itemList:['保存到本地'],
-      success:function(res){
-        if(!res.cancel){
-          if(res.tapIndex==0){
+      itemList: ['保存到本地'],
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
             wx.showToast({
-              title:'下载中...',
-              duration:50000,
-              icon:'loading'
+              title: '下载中...',
+              duration: 50000,
+              icon: 'loading'
             })
             wx.downloadFile({
               url: e.currentTarget.dataset.src,
               type: 'image', // 下载资源的类型，用于客户端识别处理，有效值：image/audio/video
               // header: {}, // 设置请求的 header
-              success: function(ress){
-                 wx.saveFile({
-                    tempFilePath: ress.tempFilePath,
-                    success: function(resl){
-                      console.log(resl);
-                      wx.hideToast();
-                      wx.showToast({
-                        title:'保存成功',
-                        icon:'success',
-                        duration:1000
-                      })
-                    },
-                    fail:function(resx){
-                      console.log("失败");
-                      console.log(res);
+              success: function (ress) {
+                wx.saveFile({
+                  tempFilePath: ress.tempFilePath,
+                  success: function (resl) {
+                    console.log(resl);
+                    wx.hideToast();
+                    wx.showToast({
+                      title: '保存成功',
+                      icon: 'success',
+                      duration: 1000
+                    })
+                  },
+                  fail: function (resx) {
+                    console.log("失败");
+                    console.log(res);
                   }
                 })
               },
-              fail:function(nn){
+              fail: function (nn) {
                 console.log("出错了");
                 console.log(nn);
               }
@@ -100,40 +128,40 @@ Page({
       }
     })
   },
-  showBottom:function(){
-    let that=this;
-    if(that.data.intervalOver){
+  showBottom: function () {
+    let that = this;
+    if (that.data.intervalOver) {
       return;
     }
     this.setData({
-      bottomDisplay:'block',
-      winHeight:that.data.winHeight-50,
-      intervalOver:true,
-      bottomHidden:false
+      bottomDisplay: 'block',
+      winHeight: that.data.winHeight - 50,
+      intervalOver: true,
+      bottomHidden: false
     })
-    setTimeout(function(){
+    setTimeout(function () {
       that.setData({
-        bottomDisplay:'none',
-        winHeight:that.data.winHeight+50,
-        intervalOver:false,
-        bottomHidden:true
+        bottomDisplay: 'none',
+        winHeight: that.data.winHeight + 50,
+        intervalOver: false,
+        bottomHidden: true
       })
-    },2000)
+    }, 2000)
   },
-  onReady:function(){
+  onReady: function () {
     // 页面渲染完成
   },
-  onShow:function(){
+  onShow: function () {
     // 页面显示
   },
-  onHide:function(){
+  onHide: function () {
     // 页面隐藏
   },
-  onUnload:function(){
-    if(app.globalData.finishCreateFlag){
-        wx.navigateBack({
-          delta: 6
-        })
-    } 
+  onUnload: function () {
+    if (app.globalData.finishCreateFlag) {
+      wx.navigateBack({
+        delta: 6
+      })
+    }
   }
 })
