8d9828b031566285605dba0ce527bf11bd699f14
ycl <y20080328@126.com>
2017-01-03 19:28:51 +0800

    style ok

--- dream-album-lite/pages/createliteview/createlite.js
+++ dream-album-lite/pages/createliteview/createlite.js
@@ -23,22 +23,17 @@ let pageData = {
     icon_left: 0,
     created: false,
     hiddenGrid: false, // 是否显示预览(四宫格)图 true 显示四宫格,false显示创建预览页
-    currentPage: 0, // 创建面板上, 当前处理的页面 起始为0
+    // currentPage: 0, // 创建面板上, 当前处理的页面 起始为0
     photoCount: 0,   // 当前相册所有的照片的数量
     scrollLeft: 0,
-    pageAnimCur: {},
-    pageAnimR:{},
-    pageAnimL:{},
-    pagescallable: true
+    pageAnim: {}
   },
   onLoad: function (option) {
-    wx.setStorageSync('userId', 19)
     // 读取传入和本地数据
     this.anim = wx.createAnimation({
       transformOrigin:"0 0 0",
       duration: 10,
     })
-    this.animP= wx.createAnimation({duration:0});
     this.init()
   },
   initAlbumDetail: function (index) {
@@ -119,7 +114,7 @@ let pageData = {
       },
       method: 'GET',
       success: function (res) {
-
+        console.log(res)
         let alist = that.data.albumList.concat(res.data)
         // alist = alist.concat(res.data)
         that.setData({
@@ -192,7 +187,6 @@ let pageData = {
     let photo = this.tmpPhotoList[index]
 
     if (photo.elesrc != "" && photo.elesrc != undefined) {
-
       wx.uploadFile({
         url: app.globalData.serverHost + "/dream/album/lite/common/uploaduserimg.json",
         filePath: photo.elesrc,
@@ -201,29 +195,30 @@ let pageData = {
           'userId': wx.getStorageSync('userId') + "",
           'albumItemId': photo.albumItemId + "",
           'albumId': albumId + "",
-          'index':(photo.rank)+"",
+          'index':photo.rank,
           'appId': app.globalData.appId + "",
-          'cssElmMoveX': Math.round(photo.cssShowX/photo.rateWidth)+"",
-          'cssElmMoveY': Math.round(photo.cssShowY/photo.rateHeight)+"",
-          'cssElmWidth': Math.round(photo.cssShowWidth/photo.rateWidth)+"",
-          'cssElmHeight': Math.round(photo.cssShowHeight/photo.rateHeight)+"",
-          'cssElmRotate': Math.round(photo.cssElmRotate)+"",
-          'isComplete': (index == this.tmpPhotoList.length-1 ? 1: 0)+""
+          'cssElmMoveX': photo.cssShowX/photo.rateWidth,
+          'cssElmMoveY': photo.cssShowY/photo.rateHeight,
+          'cssElmWidth': photo.cssShowWidth/photo.rateWidth,
+          'cssElmWidth': photo.cssShowHeight/photo.rateHeight,
+          'cssElmRotate': photo.cssElmRotate,
+          'isCompelete': index == this.tmpPhotoList.length-1
         },
         fail: function (res) {
           wx.hideToast()
           wx.showModal({
-            title: "提示1",
+            title: "提示",
             content: "上传文件错误，请从新上传",
             showCancel: false
           })
         },
         success: function (res) {
+          console.log("upload img:",res)
           let jsdata = JSON.parse(res.data);
           if (jsdata.status != 0) {
             wx.hideToast()
             wx.showModal({
-              title: "提示2",
+              title: "提示",
               content: "上传文件错误，请从新上传",
               showCancel: false
             })
@@ -245,22 +240,22 @@ let pageData = {
           'albumId': albumId + "",
           'index':photo.rank,
           'appId': app.globalData.appId + "",
-          'isComplete': (index == this.tmpPhotoList.length-1 ? 1: 0)+""
+          'isCompelete': index == this.tmpPhotoList.length-1
         },
         fail: function (res) {
           wx.hideToast()
           wx.showModal({
-            title: "提示3",
+            title: "提示",
             content: "上传文件错误，请从新上传",
             showCancel: false
           })
         },
         success: function (res) {
-
+          console.log("upload no img:",res)
           if (res.data.status != 0) {
             wx.hideToast()
             wx.showModal({
-              title: "提示4",
+              title: "提示",
               content: "上传文件错误，请从新上传",
               showCancel: false
             })
@@ -277,26 +272,23 @@ let pageData = {
     }
   },
   createAlbum: function(e){
-    // this.setData({
-    //   scrollLeft: this.data.scrollLeft+10
-    // })
-    // 照片样式调整完成后上传
-    wx.showToast({
-      title: '正在上传照片...',
-      icon: 'loading',
-      duration: 10000
+    this.setData({
+      scrollLeft: this.data.scrollLeft+10
     })
-    // 将所有photolist 提取出来
-    this.tmpPhotoList = []
-    let pagelist = this.getPageList()
-
-    for(let i=0; i<pagelist.length; i++){
-      let tmpplist = pagelist[i]
-      this.tmpPhotoList = this.tmpPhotoList.concat(tmpplist.photoInfos)
-    }
-
-    // uploadimg
-    this.uploadImage(0)
+    // // 照片样式调整完成后上传
+    // wx.showToast({
+    //   title: '正在上传照片...',
+    //   icon: 'loading',
+    //   duration: 10000
+    // })
+    // // 将所有photolist 提取出来
+    // this.tmpPhotoList = []
+    // for(let i=0; i<this.data.pageList.length; i++){
+    //   let tmpplist = this.data.pageList[i]
+    //   this.tmpPhotoList.concat(tmpplist.photoInfos)
+    // }
+    // // uploadimg
+    // this.uploadImage(0)
   },
   chooseImageList: function(e){
     let that = this;
@@ -338,6 +330,7 @@ let pageData = {
     for(let i =0; i< this.getPageList().length; i++){
         this.initPageData(i)
     }
+    console.log(this.getPageList())
     this.setData({
       pageList : this.getPageList()
     })
@@ -351,11 +344,11 @@ let pageData = {
     let photoList = this.getPhotoList(index)
 
     // 根据page wh 和 photo wh 计算 显示到屏幕上的width height, css x,y,rotate
-    let pageOriHeight = page.imgHeight
-    let pageOriWidth = page.imgWidth
+    let pageOriHeight = page.imgWidth
+    let pageOriWidth = page.imgHeight
     let rateWidth = this.data.pageFullWidth / pageOriWidth
     let rateHeight = this.data.pageFullHeight / pageOriHeight
-    // console.log("pageOriHeight",pageOriHeight, "pageOriWidth",pageOriWidth, "rateWidth",rateWidth,"rateHeight",rateHeight,"pageFullWidth",this.data.pageFullWidth, "pageFullHeight", this.data.pageFullHeight)
+
     for(let i = 0; i<photoList.length; i++){
       let photo = photoList[i]
       photo.rateWidth = rateWidth
@@ -364,7 +357,7 @@ let pageData = {
       photo.cssShowHeight = photo.cssElmHeight * rateHeight
       photo.cssShowX = photo.cssElmMoveX * rateWidth
       photo.cssShowY = photo.cssElmMoveY * rateHeight
-      this.anim.translate(photo.cssShowX, photo.cssShowY).scale(photo.cssShowWidth/100,photo.cssShowHeight/100).rotate(0).step()
+      this.anim.translate(100*i, 100*i).scale(1.5,1.5).rotate(0).step()
       let transformData = this.anim.export()
       photo.transformShadow = transformData;  // 前面阴影部分的动画
       photo.transformImg = transformData;   // 后端图片部分的动画
@@ -386,74 +379,32 @@ let pageData = {
     // this.gotoPage(this.data.currentPage -1)
   },
   chooseImage: function (e) {
-
-    let index = e.target.dataset.index
-    let idx = e.target.dataset.idx // page idx
-
-    let photoList = this.getPhotoList(idx)
-    let photo = photoList[index]
-    let that = this
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-        // let tmppaths = res.tempFilePaths;
-        photo.elesrc = res.tempFilePaths[0]
-        that.setData({
-          pageList: that.getPageList()
-        })
-      }
-    })
+    // let index = e.target.dataset.index
+    // let photo = this.getPhotoList()[index]
+    // let that = this
+    // wx.chooseImage({
+    //   count: 1,
+    //   sizeType: ['original', 'compressed'],
+    //   sourceType: ['album', 'camera'],
+    //   success: function (res) {
+    //     // let tmppaths = res.tempFilePaths;
+    //     photo.elesrc = res.tempFilePaths[0]
+    //     that.setData({
+    //       photoList: that.getPhotoList()
+    //     })
+    //   }
+    // })
   },
   touchstart: function(e){
-
-    this.touchindex = e.target.dataset.index
-    this.touchidx = e.target.dataset.idx // page idx
-
     this.touches = e.touches
-    this.setData({
-      pagescallable: false
-    })
-    this.tmpx = 0;
-    this.tmpy = 0;
-    this.scalex = 1;
-    this.scaley = 1;
+
   },
   touchmove: function(e){
-    let that = this
     let touches = e.touches
-    let photo = this.getPhotoList(this.touchidx)[this.touchindex]
-    if(touches.length <=1){
-      // move
-      this.tmpx = touches[0].pageX - this.touches[0].pageX
-      this.tmpy = touches[0].pageY - this.touches[0].pageY
-
-      this.anim.translate(this.tmpx+photo.cssShowX, this.tmpy+photo.cssShowY).scale(photo.cssShowWidth/100,photo.cssShowHeight/100).step();
-      photo.transformImg = this.anim.export();
-      this.setData({
-        pageList: that.getPageList()
-      })
-    }else{
-      this.scalex = (touches[0].pageX - touches[1].pageX)/ (this.touches[0].pageX - this.touches[1].pageX) * 0.5
-      this.scaley = (touches[0].pageY - touches[1].pageY)/ (this.touches[0].pageY - this.touches[1].pageY) * 0.5
-      this.tmpShowWidth = photo.cssShowWidth *(0.5 + this.scalex * 0.5)
-      this.tmpShowHeight = photo.cssShowHeight *(0.5 + this.scaley * 0.5)
-      this.anim.translate(photo.cssShowX, photo.cssShowY).scale(this.tmpShowWidth/100, this.tmpShowWidth/100)
-    }
+    console.log(e)
   },
   touchend: function(e){
-    let photo = this.getPhotoList(this.touchidx)[this.touchindex]
-    if(e.touches.length <=1){
-      photo.cssShowX = this.tmpx+photo.cssShowX
-      photo.cssShowY = this.tmpy+photo.cssShowY
-    }else{
-      photo.cssShowWidth = this.tmpShowWidth
-      photo.cssShowHeight = this.tmpShowWidth
-    }
-    this.setData({
-      pagescallable: true
-    })
+
   },
   getPageList: function(){
     return this.data.albumList[this.data.choosed].albumItemList
@@ -468,18 +419,30 @@ let pageData = {
     this.data.albumList[this.data.choosed].albumItemList = pageList
   },
   refreshData: function(){
+    // 根据choosed 和 currentpage 刷新 pagelist 和 photolist
+    // let pageList = this.data.albumList[this.data.choosed].albumItemList
+    // this.setData({
+    //   pageList: pageList,
+    //   photoList: pageList[this.data.currentPage].photoInfos
+    // })
   },
   refreshPage: function(){
-
+    // this.setData({
+    //   pageList: this.data.albumList[this.data.choosed].albumItemList
+    // })
   },
   refreshPhoto: function(){
+    // let pageList = this.data.albumList[this.data.choosed].albumItemList
+    // this.setData({
+    //   photoList: pageList[this.data.currentPage].photoInfos
+    // })
   },
   scrollStart: function(e){
     this.scrollPage = e.touches[0]
     this.tmpScrollLeft = this.data.scrollLeft
   },
   scrollMove: function(e){
-
+    // console.log(e)
     let p0 = e.touches[0]
     let tmpLeft = this.scrollPage.pageX - p0.pageX
     let scrollLeft = this.tmpScrollLeft + tmpLeft
@@ -493,23 +456,16 @@ let pageData = {
     })
   },
   scrollEnd: function(e){
-
+    console.log(e)
   },
   scrollPage: function(e){
-
+    console.log(e)
     let scrollLeft = e.detail.scrollLeft
-    let currentPage = Math.floor(scrollLeft/this.data.content_width)
-    let rltMovx = scrollLeft - currentPage * this.data.content_width;
-    let anc = {}
-    anc.tranx = 100- rltMovx /this.data.content_width * 100
-    anc.scale = 0.7+ 0.3*(rltMovx/this.data.content_width)
-
-    this.animP.translate(-anc.tranx, 0).scale(anc.scale).step();
+    let scale = 1- scrollLeft / 1500
+    this.anim.translate(0, 0).scale(scale).rotate(0).step();
     this.setData({
-      currentPage: currentPage,
-      pageAnimR: this.animP.export()
+      pageAnim: this.anim.export()
     })
-
   }
 }
 Page(pageData)
