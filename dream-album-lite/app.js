c8de0a693ea1d69293ebe37297894e6ad41a743e
ycl <y20080328@126.com>
2017-01-05 10:57:17 +0800

    app.js

--- dream-album-lite/app.js
+++ dream-album-lite/app.js
@@ -13,9 +13,9 @@ App({
   globalData:{
     windowWidth: 375,
     windowHeight: 625,
-    serverHost: "https://api.mokous.com/wx/",
-    //http://10.1.0.131:8080/dream-album/",
-     //https://developer.mokous.com/wx/",
+    serverHost: "https://developer.mokous.com/wx/",
+    // serverHost: "http://10.1.0.131:8080/dream-album/",
+    // serverHost: "https://developer.mokous.com/wx/",
     finishCreateFlag:false,
     albumPageCount: 4,
     appId: "wxdfbe82261831dda7"
