b665905e39f14b2b13f41e34d860c3444893abb9
ycl <y20080328@126.com>
2017-01-10 17:15:28 +0800

    调整文字显示

--- dream-space/pages/discovery/discovery.js
+++ dream-space/pages/discovery/discovery.js
@@ -4,7 +4,7 @@ let pageData = {
     },
     onLoad:function(option){
         let activitylist = [
-            {id:0,title:"标题1",description:"活动1描述"},
+            {id:0,title:"标题1标题1标题1标题1标题1标题1标题1",description:"在分析前，我们要确认下这个游戏能不能汉化，安装游戏，在pak文件中搜索相关的游戏文本，然后修改掉，然后进到游戏中查看，如果不能正常显示，那么我们还要考虑下字体问题"},
             {id:0,title:"标题2",description:"活动2描述"},
             {id:0,title:"标题3",description:"活动3描述"},
             {id:0,title:"标题4",description:"活动4描述"}]
