b64362b09630dc6baeaf31494ddada1547b39a19
ycl <y20080328@126.com>
2017-01-19 19:44:37 +0800

    调整投票页到详情

--- dream-space/pages/vote/vote.js
+++ dream-space/pages/vote/vote.js
@@ -197,18 +197,6 @@ Page({
     }
 
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  },
   voteforone:function(){
     let that = this;
     this.voting = true;
